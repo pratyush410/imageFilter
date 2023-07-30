c52d9adec877e3c20df0f670410a9a6210953870
pratyush410 <124598286+pratyush410@users.noreply.github.com>
2023-07-30 13:41:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(640,480)
+canvas.position(110,250)
+video=createCapture(VIDEO)
+video.hide()
+tintColor=""
+}
+
+function draw(){
+image(video,0,0,640,480)
+tint(tintColor)
+}
+
+function filterTint (){
+    tintColor=document.getElementById("color").value
+}
+
+function take_snapshot(){
+    save("snapshot.jpeg")
+}
